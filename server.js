7a19bb9f2b0bf8cf48ce058df99011c3342d6d07
Ahmed Karasneh <karasneh94@gmail.com>
2021-04-20 13:25:17 +0300

    exam

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const pg = require('pg');
 const methodOverride = require('method-override');
 const superagent = require('superagent');
 const cors = require('cors');
+const { request, response } = require('express');
 
 // Environment variables
 require('dotenv').config();
@@ -15,6 +16,10 @@ const PORT = process.env.PORT || 3000;
 
 // Express middleware
 // Utilize ExpressJS functionality to parse the body of the request
+app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
 
 // Specify a directory for static resources
 
@@ -32,8 +37,50 @@ const client = new pg.Client(process.env.DATABASE_URL);
 // -- WRITE YOUR ROUTES HERE --
 
 
+app.get('/', homepage);
+
+app.get('/favorite-quotes', showFav);
+
+app.post('/fav', addFavourite);
+
 // callback functions
 // -- WRITE YOUR CALLBACK FUNCTIONS FOR THE ROUTES HERE --
+
+
+function homepage(req, res) {
+    let url = `https://thesimpsonsquoteapi.glitch.me/quotes?count=10`
+
+    superagent.get(url).set('User-Agent', '1.0').then(resp =>{
+
+res.render('./pages/index.ejs' , {data: resp.body});
+    }).catch(err => console.log(err));
+}
+
+
+function addFavourite(req, res) {
+    let values = [req.body.char , req.body.quote, req.body.image, req.body.dir] 
+    
+    let sql = `INSERT INTO fav(simCharacter, quote, simImage, direction)
+    VALUES($1, $2 , $3 , $4);`
+client.query(sql, values).then(re => {
+    res.send('/');
+
+});
+
+
+
+}
+
+
+function showFav(req, res) {
+
+    let sql = `SELECT * FROM fav`;
+    client.query(sql).then(re => {
+        res.render('./pages/favs.ejs' , {data: re});
+    });
+    
+    
+}
 
 // helper functions
 
